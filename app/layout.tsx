c9b8a69b708699b553f586ded35c6e08d02fd5c7
Hamza Qureshi <hazq96@gmail.com>
2025-12-30 09:50:51 -0600

    Adding vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 
 import type React from "react"
 import type { Metadata } from "next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
